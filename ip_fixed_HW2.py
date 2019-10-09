e5f5a356602f11a7cd801d9dc29dc49ddc36faf2
iperrilles <isaac-perrilles@uiowa.edu>
2019-10-09 16:57:36 -0500

    Cleaned-up copy -IP

--- ip_fixed_HW2.py
+++ ip_fixed_HW2.py
@@ -1,5 +1,9 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
+'''
+DiceGame created 10/9/19
+@ author: Isaac Perrilles, Liz Swanson, Jessica Tse
+'''
+
 def dicegame(numsim):
     import random
     averagewon = 0
@@ -42,42 +46,3 @@ is usually close to 5. This being said, you still have good odds of making a pro
 $2 on average per game. Additionally, the max won that I received was close to $70, 
 which is a great best case outcome for a $3 buy-in.
 =======
-# -*- coding: utf-8 -*-
-def dice(numsim):
-    import random
-    averagewon = 0
-    gamewinnings = 0
-    winninglist = []
-    runtime = 0
-    maxwon = 0
-    
-    while runtime < numsim:
-        roll = random.randint(1,6)
-        runtime += 1
-        if roll <= 3:
-            gamewinnings += 0;
-        elif roll == 4:
-            gamewinnings += roll
-        elif roll == 5:
-            gamewinnings += roll
-        elif roll == 6:
-            gamewinnings += roll
-        winninglist.append(gamewinnings)
-        
-    averagewon = sum(winninglist)/len(winninglist)
-    maxwon = max(winninglist)
-    total = sum(winninglist)
-    
-    print("Average amount won= "+ "{0:.2f}".format(averagewon))
-    print("Max amount won= " + str(maxwon))   
-    return averagewon, maxwon  
-    
-dice(10000)
-
-'''
-For $3 a game, I would not play. With playing 10,000 different games at $3/game,
-it'll cost you $30,000. When running 10,000 through my program, on average I would 
-win close to $12000 with a max close to $25,000. This means that even when I do my best,
-I still will be in the hole.
->>>>>>> 60cc574c995a8b1bf701b3372afef18a7fc9061f
-'''
